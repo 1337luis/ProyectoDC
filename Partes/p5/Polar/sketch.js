349097cc141237b2e5afc136e56c4b2b51f4a1df
Luis Santos <luis1337@outlook.es>
2019-02-08 13:22:21 +0100

    Polar rose started

--- Partes/p5/Polar/sketch.js
+++ Partes/p5/Polar/sketch.js
@@ -1,8 +1,25 @@
-function setup() {
-  // put setup code here
+var drawing = true;
+function setup()
+{
+  createCanvas(400, 400);
+  drawRose(8, 5);
 }
 
-function draw() {
-  // put drawing code here
-  ellipse(50, 50, 80, 80);
+
+function drawRose( n, d )
+{
+	clear();
+	var k = n / d;
+    translate(width / 2, height / 2);
+    beginShape();
+    stroke(0);
+    noFill();
+    strokeWeight(1);
+    for (var a = 0; a < TWO_PI * d; a += 0.02) {
+      var r = 200 * cos(k * a);
+      var x = r * cos(a);
+      var y = r * sin(a);
+      vertex(x, y);
+    }
+    endShape(CLOSE);
 }
